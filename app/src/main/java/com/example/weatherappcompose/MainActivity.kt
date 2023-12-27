926ce1df12e8dbb1caa6496b71cd6b92f09274a8
Badizawr <vicodin-13@ya.ru>
2023-12-28 00:00:26 +0500

    created the basic markup

--- app/src/main/java/com/example/weatherappcompose/MainActivity.kt
+++ app/src/main/java/com/example/weatherappcompose/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.weatherappcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.weatherappcompose.ui.theme.WeatherAppComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,8 +32,43 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    Greeting("Android")
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(modifier = Modifier.fillMaxHeight(0.5f)
+            .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Temp in $name")
+        }
+        Box(modifier = Modifier.fillMaxHeight()
+            .fillMaxWidth(),
+            contentAlignment = Alignment.BottomCenter
+            ) {
+            Button(onClick = {
+
+            }, modifier = Modifier.padding(5.dp)
+                .fillMaxWidth()
+                ) {
+                Text(text = "Refresh",)
+            }
+
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WeatherAppComposeTheme {
+        Greeting("Android")
     }
 }
