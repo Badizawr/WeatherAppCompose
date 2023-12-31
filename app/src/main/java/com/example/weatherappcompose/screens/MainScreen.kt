86a2bc5c362cca924571f6b8a89273fb649e7431
Badizawr <vicodin-13@ya.ru>
2023-12-31 22:33:33 +0500

    finished the layout of the top card

--- app/src/main/java/com/example/weatherappcompose/screens/MainScreen.kt
+++ app/src/main/java/com/example/weatherappcompose/screens/MainScreen.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -33,10 +35,10 @@ import com.example.weatherappcompose.ui.theme.BlueLight
 fun MainScreen() {
     Image(
         painter = painterResource(id = R.drawable.weather_bg),
-        contentDescription = "img_1",
+        contentDescription = "img1",
         modifier = Modifier
             .fillMaxSize()
-            .alpha(0.6f),
+            .alpha(0.5f),
         contentScale = ContentScale.Crop
     )
     Column(
@@ -47,6 +49,7 @@ fun MainScreen() {
         Card(
             modifier = Modifier
                 .fillMaxWidth()
+                .alpha(0.75f)
                 .background(BlueLight),
             elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
             shape = RoundedCornerShape(10.dp)
@@ -61,15 +64,57 @@ fun MainScreen() {
                 ) {
                     Text(
                         modifier = Modifier.padding(top = 8.dp, start = 10.dp),
-                        text = "31 December 2023 13:00 ",
+                        text = "31 December 2023/13:00 ",
                         style = TextStyle(fontSize = 15.sp),
                         color = Color.Black
                     )
                     AsyncImage(
                         model = "https://cdn.weatherapi.com/weather/64x64/night/293.png",
                         contentDescription = "img2",
-                        modifier = Modifier.size(45.dp)
-                            .padding(top = 2.dp, end = 10.dp))
+                        modifier = Modifier
+                            .size(45.dp)
+                            .padding(top = 2.dp, end = 10.dp)
+                    )
+                }
+                Text(
+                    text = "London",
+                    style = TextStyle(fontSize = 20.sp),
+                    color = Color.Black
+                )
+                Text(
+                    text = "23°C",
+                    style = TextStyle(fontSize = 65.sp),
+                    color = Color.Black
+                )
+                Text(
+                    text = "Cloudly",
+                    style = TextStyle(fontSize = 18.sp),
+                    color = Color.Black
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.baseline_search),
+                            contentDescription = "img3",
+                            tint = Color.Black
+                        )
+                    }
+                    Text(
+                        modifier = Modifier.padding(top = 8.dp),
+                        text = "12°C / 23°C",
+                        style = TextStyle(fontSize = 16.sp),
+                        color = Color.Black
+                    )
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.baseline_autorenew),
+                            contentDescription = "img4",
+                            tint = Color.Black
+                        )
+                    }
                 }
             }
         }
